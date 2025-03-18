341a17fa48b491deb8dffca35333ee909cb8d4d4
Jonathan Math <jonathanamath@gmail.com>
2025-03-18 15:47:16 -0400

    add forcedelete to registry so it can be brought up and taken down easily

--- _infrastructure/terraform/stacks/registry/ecr.tf
+++ _infrastructure/terraform/stacks/registry/ecr.tf
@@ -2,6 +2,7 @@
 resource "aws_ecr_repository" "portfolio_repo" {
   name                 = "portfolio-app"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   encryption_configuration {
     encryption_type = "AES256"
