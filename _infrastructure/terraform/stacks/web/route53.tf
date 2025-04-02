b143f0e377fa7de1cfbfc23afa0e8aa6e3fc8f9e
Jonathan Math <jonathanamath@gmail.com>
2025-04-02 15:47:32 -0400

    clean up hardcoded value

--- _infrastructure/terraform/stacks/web/route53.tf
+++ _infrastructure/terraform/stacks/web/route53.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "ec2_dns" {
   count = var.use_vpc ? 0 : 1
 
   zone_id = data.terraform_remote_state.domain.outputs.route53_zone.id
-  name    = "api.jonathanmath.com"
+  name    = "api.${var.domain_name}.${var.tld}"
   type    = "A"
 
   ttl     = 300
