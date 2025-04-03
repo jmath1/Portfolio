398aca6dd39d6a40075a89ba5853b47a757c7a82
Jonathan Math <jonathanamath@gmail.com>
2025-04-03 18:54:09 -0400

    change secrets to allow force destroy (no more messy destructions from secrets pending deletion) and change rds_hostname secret to 127.0.0.1 when we arent using rds (we will be hitting a docker container that lives on ec2 in host network mode)

--- _infrastructure/terraform/stacks/web/secrets.tf
+++ _infrastructure/terraform/stacks/web/secrets.tf
@@ -1,9 +1,8 @@
 resource "aws_secretsmanager_secret" "env_secrets" {
   for_each = local.env_vars
   name     = "${each.key}"
-  lifecycle {
-    prevent_destroy = true
-  }
+  force_overwrite_replica_secret = true
+  recovery_window_in_days  = 0
 }
 
 resource "aws_secretsmanager_secret_version" "env_secrets_version" {
@@ -11,76 +10,58 @@ resource "aws_secretsmanager_secret_version" "env_secrets_version" {
   secret_id   = each.value.id
   secret_string = local.env_vars[each.key]
 
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 resource "aws_secretsmanager_secret" "rds_hostname" {
     name = "DB_HOST"
-
+    force_overwrite_replica_secret = true
+    recovery_window_in_days  = 0
 }
 
 resource "aws_secretsmanager_secret_version" "rds_hostname_version" {
     secret_id   = aws_secretsmanager_secret.rds_hostname.id
-    secret_string = aws_db_instance.portfolio_db.address
+    secret_string = var.use_rds ? aws_db_instance.portfolio_db[0].address : "127.0.0.1"
 
 }
 
 resource "aws_secretsmanager_secret" "rds_port" {
     name = "DB_PORT"
     force_overwrite_replica_secret = true
-    lifecycle {
-        prevent_destroy = true
-    }
+    recovery_window_in_days  = 0
 }
 
 resource "aws_secretsmanager_secret_version" "rds_port_version" {
     secret_id   = aws_secretsmanager_secret.rds_port.id
-    secret_string = aws_db_instance.portfolio_db.port
-    lifecycle {
-        prevent_destroy = true
-    }
+    secret_string = var.use_rds ? aws_db_instance.portfolio_db[0].port : 5432
 }
 
 resource "aws_secretsmanager_secret" "allowed_host" {
     name = "ALLOWED_HOST"
-    force_overwrite_replica_secret = true
-    lifecycle {
-        prevent_destroy = true
-    }
+    force_overwrite_replica_secret = true    
+    recovery_window_in_days  = 0
 }
 
 resource "aws_secretsmanager_secret_version" "alloweed_host_version" {
     secret_id   = aws_secretsmanager_secret.allowed_host.id
     secret_string = "api.${var.domain_name}.${var.tld}"
-    lifecycle {
-        prevent_destroy = true
-    }
 }
 
 resource "aws_secretsmanager_secret" "bucket_arn" {
     name = "S3_BUCKET_ARN"
     force_overwrite_replica_secret = true
-    lifecycle {
-        prevent_destroy = true
-    }
+    recovery_window_in_days  = 0
 }
 
 resource "aws_secretsmanager_secret_version" "bucket_arn_version" {
     secret_id   = aws_secretsmanager_secret.bucket_arn.id
     secret_string = aws_s3_bucket.portfolio_bucket.arn
-    lifecycle {
-        prevent_destroy = true
-    }
+
 }
 
 resource "aws_secretsmanager_secret" "private_ec2_ip" {
     name = "PRIVATE_IP"
     force_overwrite_replica_secret = true
-    lifecycle {
-        prevent_destroy = true
-    }
+    recovery_window_in_days  = 0
 }
 
 resource "aws_secretsmanager_secret_version" "private_ec2_ip_version" {
