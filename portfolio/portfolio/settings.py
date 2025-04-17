e79e3364dbf551c9aa920c5dda9d78210251e543
Jonathan Math <jonathanamath@gmail.com>
2025-04-16 22:50:14 -0400

    update settings to work out cors issue

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -129,6 +129,22 @@ if os.getenv("CLOUD"):
         }
     }
     CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS
+    
+    CORS_ALLOW_METHODS = [
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+        "OPTIONS",
+    ]
+    CORS_ALLOW_HEADERS = [
+        "content-type",
+        "authorization",
+        "x-csrftoken",
+    ]
+    CORS_ALLOW_CREDENTIALS = True
+    
     if DEBUG:
         CORS_ALLOWED_ORIGINS = [
             "http://localhost:3000",
